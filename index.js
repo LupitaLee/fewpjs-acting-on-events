711752959b5dbaeccec88de72a248eb15c8e1531
Lupita <lupitarivera8899@gmail.com>
2021-06-16 14:10:50 -0500

    commit tree 5158835c8e6c4d7e113609c4d83e9a82b72b4ab0

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ let dodger = document.getElementById("dodger");
 dodger.style.backgroundColor = "#FF69B4";
 
 
+
+
 document.addEventListener("keydown", function(e) {
     if (e.key === "ArrowLeft") {
       moveDodgerLeft();
